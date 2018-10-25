1d6e9a64fdfd1b42df3d53ab1f26d60086453290
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-25 22:04:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-var ServerID = "494284550789136384"; //اي دي السيرفر
-var ChannelID = "494543828397719563";// اي دي الروم
-const prefix = '5'
+var ServerID = "505104693018755072"; //اي دي السيرفر
+var ChannelID = "505104693018755074";// اي دي الروم
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
